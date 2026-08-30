d049c9f589be92c86cf63fc107e67fbf5f11e0e6
justAchillGuy8386 <nguyenkien18102004@gmail.com>
2026-08-31 03:53:51 +0700

    feat(team): add team page

---
+++ src/app/team/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import { TeamAnalyticsTab } from '@/components/TeamAnalyticsTab';
+
+export default function TeamAnalyticsPage() {
+  return <TeamAnalyticsTab />;
+}
